c6786a953dbe01bf381bfc3e770021f74b88e869
shi0rik0 <anguuan@outlook.com>
2024-03-01 19:22:33 +0800

    t

--- driver.go
+++ driver.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"log"
+	"strconv"
+	"sync"
 
 	"github.com/docker/go-plugins-helpers/network"
 	"github.com/vishvananda/netlink"
@@ -13,6 +15,20 @@ const (
 )
 
 type Driver struct {
+	mutex    sync.Mutex
+	vethName map[ConnectionID]string
+	counter  int
+}
+
+type ConnectionID struct {
+	NetworkID  string
+	EndpointID string
+}
+
+func NewDriver() *Driver {
+	return &Driver{
+		vethName: make(map[ConnectionID]string),
+	}
 }
 
 func (d *Driver) GetCapabilities() (*network.CapabilitiesResponse, error) {
@@ -58,29 +74,80 @@ func (d *Driver) EndpointInfo(request *network.InfoRequest) (*network.InfoRespon
 func (d *Driver) Join(request *network.JoinRequest) (*network.JoinResponse, error) {
 	log.Printf("Join(): %v\n", request)
 
-	name1 := "veth-test-7"
-	name2 := "veth-test-8"
-	linkAttr := netlink.NewLinkAttrs()
-	linkAttr.Name = name1
-	veth := &netlink.Veth{LinkAttrs: linkAttr, PeerName: name2}
-	err := netlink.LinkAdd(veth)
-	if err != nil {
-		fmt.Print(err)
-	}
-	if1, _ := netlink.LinkByName(name1)
-	if2, _ := netlink.LinkByName(name2)
-	netlink.LinkSetUp(if1)
-	netlink.LinkSetUp(if2)
+	d.mutex.Lock()
+	defer d.mutex.Unlock()
 
+	cid := ConnectionID{NetworkID: request.NetworkID, EndpointID: request.EndpointID}
+	if _, ok := d.vethName[cid]; ok {
+		return nil, errors.New("connection exists")
+	}
+
+	id := strconv.Itoa(d.counter)
+	name1 := "veth-ebpf-" + id
+	name2 := "veth-temp-" + id
+	err := createVeth(name1, name2)
+	if err != nil {
+		return nil, err
+	}
+
+	d.vethName[cid] = name1
+	d.counter++
 	return &network.JoinResponse{InterfaceName: network.InterfaceName{
 		SrcName:   name2,
 		DstPrefix: "eth",
 	}}, nil // TODO: impl
 }
 
+func createVeth(name1 string, name2 string) error {
+	linkAttr := netlink.NewLinkAttrs()
+	linkAttr.Name = name1
+	veth := &netlink.Veth{LinkAttrs: linkAttr, PeerName: name2}
+	err := netlink.LinkAdd(veth)
+	if err != nil {
+		return err
+	}
+	if1, err := netlink.LinkByName(name1)
+	if err != nil {
+		return err
+	}
+	if2, err := netlink.LinkByName(name2)
+	if err != nil {
+		return err
+	}
+	netlink.LinkSetUp(if1)
+	netlink.LinkSetUp(if2)
+	return nil
+}
+
+func deleteVeth(name string) error {
+	link, err := netlink.LinkByName(name)
+	if err != nil {
+		return err
+	}
+	err = netlink.LinkDel(link)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (d *Driver) Leave(request *network.LeaveRequest) error {
-	// 只需要删除host一侧的veth网卡即可。
 	log.Printf("Leave(): %v\n", request)
+
+	d.mutex.Lock()
+	defer d.mutex.Unlock()
+
+	cid := ConnectionID{NetworkID: request.NetworkID, EndpointID: request.EndpointID}
+	name, ok := d.vethName[cid]
+	if !ok {
+		return errors.New("connection doesn't exist")
+	}
+
+	err := deleteVeth(name)
+	if err != nil {
+		return err
+	}
+
 	return nil // TODO: impl
 }
 
@@ -105,8 +172,8 @@ func (d *Driver) RevokeExternalConnectivity(request *network.RevokeExternalConne
 }
 
 func main() {
-
-	handler := network.NewHandler(&Driver{})
+	driver := NewDriver()
+	handler := network.NewHandler(driver)
 	err := handler.ServeUnix(PLUGIN_NAME, 0)
 	if err != nil {
 		log.Fatal(err)
