968800664c36a7419f81b25230a6255e82b0a60c
shi0rik0 <anguuan@outlook.com>
2024-03-11 11:19:15 +0800

    t

--- ebpf/tc.c
+++ ebpf/tc.c
@@ -4,6 +4,9 @@
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
 
+#define likely(x)		__builtin_expect(!!(x), 1)
+#define unlikely(x)		__builtin_expect(!!(x), 0)
+
 #define TC_ACT_OK 0
 #define ETH_P_IP 0x0800
 #define ETH_P_ARP 0x0806
@@ -20,14 +23,14 @@ int tc_ingress(struct __sk_buff *ctx)
 {
 	void *data_end = (void *)(uintptr_t)ctx->data_end;
 	struct ethhdr *eth_header = (struct ethhdr *)(uintptr_t)ctx->data;
-	if ((void *)(eth_header + 1) > data_end) {
+	if (unlikely((void *)(eth_header + 1) > data_end)) {
 		return TC_ACT_OK;
 	}
 	__u32 ip_addr;
 	switch (eth_header->h_proto) {
 	case bpf_htons(ETH_P_IP): {
 		struct iphdr *ip_header = (struct iphdr *)(eth_header + 1);
-		if ((void *)(ip_header + 1) > data_end) {
+		if (unlikely((void *)(ip_header + 1) > data_end)) {
 			return TC_ACT_OK;
 		}
 		ip_addr = ip_header->daddr;
@@ -35,7 +38,7 @@ int tc_ingress(struct __sk_buff *ctx)
 	}
 	case bpf_htons(ETH_P_ARP): {
 		void *arp_payload = eth_header + 1;
-		if (arp_payload + 28 > data_end) {
+		if (unlikely(arp_payload + 28 > data_end)) {
 			return TC_ACT_OK;
 		}
 		ip_addr = *(__u32 *)(arp_payload + 24);
@@ -46,7 +49,7 @@ int tc_ingress(struct __sk_buff *ctx)
 		break;
 	}
 	__u32 *ifindex = bpf_map_lookup_elem(&ip_ifindex_map, &ip_addr);
-	if (ifindex) {
+	if (likely(ifindex)) {
 		return bpf_redirect(*ifindex, 0);
 	}
 	return TC_ACT_OK;
